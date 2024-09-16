b6559514cac5cb32a79dd368ce660bc8bca437fd
HL231231 <341036663@gapps.yrdsb.ca>
2024-09-16 13:24:56 -0400

    test

--- MyWorld.java
+++ MyWorld.java
@@ -8,7 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class MyWorld extends World
 {
-
+    //Testing
     /**
      * Constructor for objects of class MyWorld.
      * 
@@ -19,7 +19,7 @@ public class MyWorld extends World
         super(600, 400, 1); 
         Hero hero = new Hero();
         addObject(hero, 100, 100);
-
+    
         Banana banana = new Banana();
         addObject(banana, 600, 100);
     }
